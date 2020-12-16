02700733dd84926494f712794c4dba965a721886
3BDO-CR7 <abdocristiano0@gmail.com>
2020-12-16 13:45:54 +0200

    new version

---
+++ ReactotronConfig.js
@@ -0,0 +1,3 @@
+import Reactotron from 'reactotron-react-native';
+
+Reactotron.configure({ host: '192.168.2.13', port: 9090 }).useReactNative().connect();
---
+++ src/consts/index.js
@@ -0,0 +1,4 @@
+export default {
+    url         : 'https://hrajelshabaka.aait-sa.com/api/v1/',
+    // url         : 'https://infinity.aait-sa.com/api/v1/',
+};
